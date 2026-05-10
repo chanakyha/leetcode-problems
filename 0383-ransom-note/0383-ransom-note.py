2d94004d8c35b9258e65671d1b92f94affaddec7
Chanakyha <vetrichanakyha2003@gmail.com>
2026-05-10 19:14:05 +0530

    Time: 15 ms (77.29%), Space: 19.8 MB (8.71%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        counter = Counter(magazine)
+
+        for c in ransomNote:
+            if c not in counter:
+                return False
+            elif counter[c] == 1:
+                del counter[c]
+            else:
+                counter[c] -= 1
+
+            
+        return True
